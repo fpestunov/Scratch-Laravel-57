88223f8034a0db270ed47e0e9ee365f1bafbca31
fpestunov <fpestunov@gmail.com>
2018-11-02 16:31:19 +0400

    Add Task Incapsulation

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -17,6 +17,12 @@ class ProjectTasksController extends Controller
         // $project->addTask(request('description'));
         $project->addTask($attributes);
 
+        // Объеденим валидацию и добавление
+        // $project->addTask(
+        //     request()->validate(['description' => 'required|min:3|max:255'])
+        // );
+
+
         // Вариант 1
         // Task::create([
         //     'project_id' => $project->id,
@@ -32,9 +38,30 @@ class ProjectTasksController extends Controller
         // dd(request()->all());
         // dd(request()->has('completed'));
 
-        $task->update([
-            'completed' => request()->has('completed')
-        ]);
+        // $task->update([
+        //     'completed' => request()->has('completed')
+        // ]);
+
+        // Encapsulation
+        // $task->complete(request()->has('completed'));
+
+        // true and false - the same?
+        // Make more readeble
+
+        // 1 вариант
+        // if (request()->has('completed')) {
+        //     $task->complete();
+        // }
+        // else {
+        //     $task->incomplete();
+        // }
+
+        // 2 вариант
+        // request()->has('completed') ? $task->complete() : $task->incomplete();
+
+        // 3 вариант
+        $method = request()->has('completed') ? 'complete' : 'incomplete';
+        $task->$method();
 
         return back();
     }
